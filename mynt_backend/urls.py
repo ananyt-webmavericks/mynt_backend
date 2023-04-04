b8f2c8f2e240f35de9e76f73c6884ed73345cc7e
shivam19977 <shivam@thesoftcoders.com>
2023-04-04 14:06:12 +0530

    media url testing

--- mynt_backend/urls.py
+++ mynt_backend/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+from django.urls import re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -33,8 +35,9 @@ urlpatterns = [
     path('api/rewards/',include("rewards.urls")),
     path('api/deal_terms/',include("deal_terms.urls")),
     path('api/documents/',include("documents.urls")),
-    path('api/payments/',include("payment.urls"))
+    path('api/payments/',include("payment.urls")),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
 
-urlpatterns = urlpatterns + \
-        static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns = urlpatterns + \
+#         static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
