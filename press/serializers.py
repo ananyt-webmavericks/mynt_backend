e696b0f2e5aba99b88ab15844285e3f92af470e4
shivam19977 <shivam@thesoftcoders.com>
2023-06-03 21:02:07 +0530

    banner list

--- press/serializers.py
+++ press/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
 from .models import Press
+import ast
 
 class Pressserializer(serializers.ModelSerializer):
+    banner_images = serializers.SerializerMethodField()
     class Meta:
         model = Press
-        fields = ["id","company_id","title","link","description","banner","created_at","updated_at"]
+        fields = ["id","company_id","title","link","description","banner","banner_images","created_at","updated_at"]
+        optional_fields = ["banner_images"]
+
+    def get_banner_images(self, obj):
+        res = ast.literal_eval(obj.banner)
+        return res
